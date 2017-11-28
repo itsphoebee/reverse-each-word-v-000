843b48f2a95212d2691f87c7ae9c341131e76b06
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 20:40:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
-
+  sentence.split.each {|word| word.reverse}
 end
+
+#method with collect
+#def reverse_each_word(sentence)
+#  sentence.split.collect {|word| word.reverse}.join(" ")
+#end
